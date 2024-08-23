88e5055eb53e90bd611c669b8ce6e11cfb575cb2
Rohit <rshivhare7405@gmail.com>
2024-08-23 23:27:50 +0530

    add form

--- signup-login/signup1.php
+++ signup-login/signup1.php
@@ -50,20 +50,20 @@ $conn->close();
 </head>
 <body>
     
+    <!-- Signup form -->
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="username">Username:</label><br>
+        <input type="text" id="username" name="username" required><br>
+        <label for="fname">First Name:</label><br>
+        <input type="text" id="fname" name="fname" required><br>
+        <label for="lname">Last Name:</label><br>
+        <input type="text" id="lname" name="lname" required><br>
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" required><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password" required><br>
+        <input type="submit" value="Signup">
+    </form>
+    
 </body>
 </html>
-
-<!-- Signup form -->
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-    <label for="username">Username:</label><br>
-    <input type="text" id="username" name="username" required><br>
-    <label for="fname">First Name:</label><br>
-    <input type="text" id="fname" name="fname" required><br>
-    <label for="lname">Last Name:</label><br>
-    <input type="text" id="lname" name="lname" required><br>
-    <label for="email">Email:</label><br>
-    <input type="email" id="email" name="email" required><br>
-    <label for="password">Password:</label><br>
-    <input type="password" id="password" name="password" required><br>
-    <input type="submit" value="Signup">
-</form>
