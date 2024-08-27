897285a5d37394b30549143aaa55efaead743682
Rohit <rshivhare7405@gmail.com>
2024-08-27 19:42:49 +0530

    update button

--- signup-login/signup1.php
+++ signup-login/signup1.php
@@ -61,7 +61,7 @@ $conn->close();
         <label for="email">Email:</label><br>
         <input type="email" id="email" name="email" required><br>
         <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" required><br>
+        <input type="password" id="password" name="password" required><br><br>
         <input type="submit" value="Signup">
     </form>
     
