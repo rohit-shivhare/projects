fbdbc81d0cab2b0411760efdb89be326b58b9508
Rohit <rshivhare7405@gmail.com>
2024-10-14 16:11:46 +0530

    Create a user function

--- github-search/index.js
+++ github-search/index.js
@@ -11,3 +11,18 @@ let main = document.getElementById("main");
 let inputForm = document.getElementById("userInput");
 let inputBox = document.getElementById("inputBox");
 
+const userGetFunction = (name) => {
+    axios(api + name) 
+        .then((Response) => {
+            userCard(Response.data);
+            repoGetFunction(name);
+        }) 
+        .catch((err) => {
+            if (
+                err.Response.status == 404
+            ) {
+                errorFunction("No profile with this username");
+            }
+        });
+}
+
